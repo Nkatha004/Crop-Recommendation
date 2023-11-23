33d0df96cfa1cfcd8f94dc6999083ac1d82d8e14
Nkatha <nkathajoy36@gmail.com>
2023-11-23 09:32:04 +0300

    Displayed sensor data

--- app/src/main/java/com/example/csproject/Parameters.kt
+++ app/src/main/java/com/example/csproject/Parameters.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.widget.SwitchCompat
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.AsyncTask
 import android.widget.EditText
